60ddd26276341aaa44ba8809a95fa44f1fe9b4ea
Filipp Pruger <filipppruger@example.com>
2026-08-05 16:14:12 +0300

    Add delete button to Order list in admin

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 from .models import Order, OrderItem
 
 class OrderItemInline(admin.TabularInline):
@@ -10,7 +12,7 @@ class OrderItemInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'country', 'paid',
-                    'status', 'created', 'updated', 'transaction_id']
+                    'status', 'created', 'updated', 'transaction_id', 'delete_button']
     list_filter = ['paid', 'created', 'updated', 'status']
     readonly_fields = ['transaction_id']
     inlines = [OrderItemInline]
@@ -20,3 +22,8 @@ class OrderAdmin(admin.ModelAdmin):
             return obj.payment.transaction_id
         return None
     transaction_id.short_description = 'Transaction ID'
+
+    def delete_button(self, obj):
+        url = reverse('admin:orders_order_delete', args=[obj.pk])
+        return format_html('<a class="button" style="color: white; background-color: #ba2121; padding: 4px 8px; border-radius: 4px; text-decoration: none;" href="{}">Удалить</a>', url)
+    delete_button.short_description = 'Действие'
